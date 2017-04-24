7121d2090ef8202378dd52f5570a3b8a20fd515f
mutahirgharay <mutahir_gharay@yahoo.com>
2017-04-24 16:56:47 +0500

    Add files via upload

---
+++ test/src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is my test project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
